4fc9c6a4a09c3d9eefe276de6a5ec6588ca05b70
shari <shari@DESKTOP-8N1CT7U>
2017-04-26 02:43:45 -0500

    First

---
+++ Example6/src/p7/Test.java
@@ -0,0 +1,12 @@
+package p7;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String s1 = "Welcome";
+		s1 = s1.concat("to");
+		s1 = s1.concat("java programming");
+		System.out.println(s1);
+	}
+
+}
