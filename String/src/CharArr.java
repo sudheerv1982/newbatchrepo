72cc8cda35331eac37ab4641a97a44e24461962c
shari <shari@DESKTOP-8N1CT7U>
2017-04-28 03:18:19 -0500

    CharArr

---
+++ String/src/CharArr.java
@@ -0,0 +1,14 @@
+
+public class CharArr {
+
+	public static void main(String[] args) {
+		String s1 = new String("java programs");
+		String s2 = "Welcome to Dallas";
+        char array[] = s1.toCharArray();
+		char arr[] = s2.toCharArray();
+		System.out.println(array);
+		System.out.println(arr);
+
+	}
+
+}
