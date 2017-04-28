d2378d1c42f4597a5ebc2cfb529fa4288f877477
shari <shari@DESKTOP-8N1CT7U>
2017-04-28 02:36:02 -0500

    Reverse

---
+++ String/src/Reverse.java
@@ -0,0 +1,13 @@
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		String s1 = "United States of America";
+		String reverse = new StringBuffer(s1).reverse().toString();
+		System.out.println("Before reverse:" +s1);
+		System.out.println("After Reverse:" +reverse);
+		
+
+	}
+
+}
