06c7642b47ed0cbc1c59053061bfa6bf722e8ce0
shari <shari@DESKTOP-8N1CT7U>
2017-04-28 02:30:32 -0500

    Lower

---
+++ String/src/StringToLower.java
@@ -0,0 +1,15 @@
+
+public class StringToLower {
+
+	public static void main(String[] args) {
+		String t3 = "I LIKE GAMES";
+		String t4 = t3.toLowerCase();
+		System.out.println("Before lowercase:" +t3);
+		System.out.println("After changing to lowercase:" +t4);
+		String s1 = new String("I AM INTERESTED IN JAVA");
+		System.out.println("The lowercase is:" +s1.toLowerCase());
+		
+
+	}
+
+}
