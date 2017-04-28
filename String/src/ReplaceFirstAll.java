94ca71bd459e08e45ee7fc694fc6be4cea49118f
shari <shari@DESKTOP-8N1CT7U>
2017-04-28 01:50:29 -0500

    ReplaceFirstAll

---
+++ String/src/ReplaceFirstAll.java
@@ -0,0 +1,12 @@
+
+public class ReplaceFirstAll {
+
+	public static void main(String[] args) {
+		String s1 = "Hello to java programming";
+		System.out.println(s1.replace("Hello", "Welcome"));
+		System.out.println(s1.replaceFirst("java", "salesforce"));
+		System.out.println(s1.replaceAll("to", "to the"));
+
+	}
+
+}
