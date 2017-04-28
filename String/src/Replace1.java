2d129f4feda8d60b4ce3b52e1adfa200fcd7c167
shari <shari@DESKTOP-8N1CT7U>
2017-04-28 02:56:35 -0500

    Replace

---
+++ String/src/Replace1.java
@@ -0,0 +1,15 @@
+
+public class Replace1 {
+
+	public static void main(String[] args) {
+     String s1 = "The quick brown fox jumps over the lazy dog.";
+     System.out.println(s1.replace('d', 'f'));
+     String s4 = "welcome to Dallas";
+		System.out.println(s4.replace("Dallas", "irving"));
+		String s5 = new String("Site is BeginnersBook.com");
+		System.out.println(s5.replace('o', 'p'));
+
+
+	}
+
+}
