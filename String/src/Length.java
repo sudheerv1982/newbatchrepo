20f12f2ac9777f22c6d40b948970584362829f59
shari <shari@DESKTOP-8N1CT7U>
2017-04-28 02:11:44 -0500

    Length

---
+++ String/src/Length.java
@@ -0,0 +1,15 @@
+
+public class Length {
+
+	public static void main(String[] args) {
+		String s1 = "Shalini";
+		String s2 = new String("Welcome to java programming");
+		String s3 = new String("Beginners Book");
+		System.out.println(s1.length());
+		System.out.println(s2.length());
+		System.out.println("Length of s3 :"  +s3.length());
+		
+
+	}
+
+}
