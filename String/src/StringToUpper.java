5c099bb848c12c7d43777df4fd21f334b8f15d81
shari <shari@DESKTOP-8N1CT7U>
2017-04-28 02:23:03 -0500

    Upper

---
+++ String/src/StringToUpper.java
@@ -0,0 +1,12 @@
+
+public class StringToUpper {
+
+	public static void main(String[] args) {
+		String s1 = "welcome to dallas";
+		String s2 = new String(s1.toUpperCase());
+		System.out.println("Before uppercase:"  +s1);
+		System.out.println("After changing to uppercase:" +s2);
+
+	}
+
+}
