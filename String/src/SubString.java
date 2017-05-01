3a1393dc89c1c492a96d51080103b984d8aa22ff
shari <shari@DESKTOP-8N1CT7U>
2017-05-01 10:13:45 -0500

    SubString

---
+++ String/src/SubString.java
@@ -0,0 +1,13 @@
+
+public class SubString {
+
+	public static void main(String[] args) {
+		String s1 = new String("quick brown fox jumps over the lazy dog");
+		System.out.println("substring starting from inex 15:");
+		System.out.println(s1.substring(15));
+		System.out.println("substring starting from index 20 and 25:");
+		System.out.println(s1.substring(20, 25));
+
+	}
+
+}
