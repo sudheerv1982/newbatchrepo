c651f1089ebeeb7d1d3e9b789a606bed8e10f005
shari <shari@DESKTOP-8N1CT7U>
2017-05-01 10:39:32 -0500

    CharAt

---
+++ String/src/CharAt.java
@@ -0,0 +1,18 @@
+
+public class CharAt {
+
+	public static void main(String[] args) {
+		String s1 = new String ("welcome to string handling tutorial");
+		char ch1 = s1.charAt(0);
+		char ch2 = s1.charAt(5);
+		char ch3 = s1.charAt(11);
+		char ch4 = s1.charAt(20);
+		System.out.println("character at 0 index is:" +ch1);
+		System.out.println("character at 5th index is:" +ch2);
+		System.out.println("character at 11th index is:" +ch3);
+		System.out.println("character at 20th index is:" +ch4);
+		
+
+	}
+
+}
