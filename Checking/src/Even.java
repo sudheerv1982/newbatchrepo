f88569947ee5f5b4ef070701b885d894e63f0ac0
shari <shari@DESKTOP-8N1CT7U>
2017-05-01 13:00:30 -0500

    Even

---
+++ Checking/src/Even.java
@@ -0,0 +1,17 @@
+
+public class Even {
+
+	public static void main(String[] args) {
+	 int j = 50;
+	 for( int i=1;i<=j;i++)
+	 {
+		 if(i%2 == 0)
+		 {
+		 System.out.println("The even Numbers are:" +i);
+
+	 }
+	 
+	}
+
+}
+}
